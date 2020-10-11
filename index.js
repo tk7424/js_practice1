7a7e9f5ba3078c7eeae1ed64999e2ef4be227e62
absolute.kagasawa <absolute.kagasawa@gmail.com>
2020-10-12 01:47:52 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+function fetchUserInfo(userId) {
+    fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+        .then(response => {
+            console.log(response.status); // => 200
+            // エラーレスポンスが返されたことを検知する
+            if (!response.ok) {
+                console.error("エラーレスポンス", response);
+            } else {
+                return response.json().then(userInfo => {
+                    // JSONパースされたオブジェクトが渡される
+                    console.log(userInfo); // => {...}
+                    const view = escapeHTML`
+                    <h4>${userInfo.name} (@${userInfo.login})</h4>
+                    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+                    <dl>
+                        <dt>Location</dt>
+                        <dd>${userInfo.location}</dd>
+                        <dt>Repositories</dt>
+                        <dd>${userInfo.public_repos}</dd>
+                    </dl>
+                    `;
+
+                    const result = document.getElementById("result");
+                    result.innerHTML = view;
+                });
+            }
+        }).catch(error => {
+            console.log(error);
+        });
+}
+
+function escapeSpecialChars(str) {
+    return str
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
+
+function escapeHTML(strings, ...values) {
+    return strings.reduce((result, str, i) => {
+        const value = values[i - 1];
+        if (typeof value === "string") {
+            return result + escapeSpecialChars(value) + str;
+        } else {
+            return result + String(value) + str;
+        }
+    });  
+}
